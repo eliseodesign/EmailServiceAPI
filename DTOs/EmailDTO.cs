58ad3b50c2d852350f5d9f8122a8e26417e38fff
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-01-11 18:39:30 -0600

    feat(DTOs): :sparkles: add EmailDTO

---
+++ DTOs/EmailDTO.cs
@@ -0,0 +1,9 @@
+namespace EmailServiceApi.DTOs
+{
+  public class EmailDTO
+  {
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Content { get; set; }
+  }
+}
